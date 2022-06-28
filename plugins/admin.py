8287e27f4254da61b661857993ba70dfc0b361c1
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-28 20:35:29 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -3,6 +3,6 @@ from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeybo
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
 from pyrogram.types.messages_and_media import message
 from pyrogram import Client
-from plugins.helpers import call_back_in_filter
+from plugins.helper_functions import call_back_in_filter
 from pyrogram import filters
 from pyrogram.types import Message
