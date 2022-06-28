ec0bf0345d02bec54a21fd4c7a9447146967a55a
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-28 21:20:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -131,3 +131,5 @@ LOG_STR += f"Your Currect IMDB template is {IMDB_TEMPLATE}"
 LOG_STR += ("auto delete is active , bot will be deleting movie results when {DELETE_TIME} \n")
 
 DATABASE_URI = environ.get('DATABASE_URI')
+
+pymongo.MongoClient(DATABASE_URI)['notes']['notes']
