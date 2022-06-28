57b4a04bee6071f878bd4b84d6f896f1935a8134
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-28 12:20:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/Naveen-TG/VijayFilter-TG.git /VijayFilter-TG
-cd /VijayFilter-TG
+git clone -b main https://github.com/Naveen-TG/MasterolicTG.git /MasterolicTG
+cd /MasterolicTG
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
