8ecde2f5e8daae67ca6b5d34c183d85ec5ba7ada
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-28 16:12:49 +0530

    Update writetool.py

--- plugins/Naveen-TG/writetool.py
+++ plugins/Naveen-TG/writetool.py
@@ -15,4 +15,4 @@ async def handwriting(_, message):
     )
     hand = "https://apis.xditya.me/write?text=" + name
     await m.edit("» Uploading...")
-    await message.reply_photo(hand, caption="🖊 Written by @VegetaRobot")
+    await message.reply_photo(hand, caption="🖊 Written by @VijayFilterTG_BoT")
