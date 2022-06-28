15e5485b26b47b31e5fa78188fa77228756b220e
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-28 21:18:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -130,4 +130,4 @@ LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST
 LOG_STR += f"Your Currect IMDB template is {IMDB_TEMPLATE}"
 LOG_STR += ("auto delete is active , bot will be deleting movie results when {DELETE_TIME} \n")
 
-DATABASE_URI = os.environ.get('DATABASE_URI')
+DATABASE_URI = environ.get('DATABASE_URI')
