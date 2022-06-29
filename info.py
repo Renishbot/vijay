bac3e36d1b3bd6998b9471ae3184ae4d82ad1d0b
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-29 19:14:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,6 +47,23 @@ class evamaria(Client):
             sleep_threshold=60
         )
 
+class Log:
+    def __init__(self, save_to_file=False, file_name="wbb.log"):
+        self.save_to_file = save_to_file
+        self.file_name = file_name
+
+    def info(self, msg):
+        print(f"[+]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[INFO]({time.ctime(time.time())}): {msg}\n")
+
+    def error(self, msg):
+        print(f"[-]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[ERROR]({time.ctime(time.time())}): {msg}\n")
+
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
