4fff2cbdbb3f725adb37bbeea002da0cd5af9b46
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-29 08:18:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -101,6 +101,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #Downloader
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 
+#greetings
+WELCOME_DELAY_KICK_SEC = WELCOME_DELAY_KICK_SEC
+
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TIGERSHROFFIMDB')
