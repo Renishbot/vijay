627d35acc98fcc44281492eedab1ae80cf3806eb
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-02 21:06:55 +0530

    Update google.py

--- plugins/Naveen/google.py
+++ plugins/Naveen/google.py
@@ -1,1 +1,15 @@
+from bs4 import BeautifulSoup
+import urllib
+from info import telethn as tbot
+import glob
+import io
+import os
+import re
+import aiohttp
+import urllib.request
+from urllib.parse import urlencode
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+from search_engine_parser import GoogleSearch
 
