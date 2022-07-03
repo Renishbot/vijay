a35bbcc19ad2868685e3c675233a0c5cd28ea43e
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-03 18:52:01 +0530

    Update __init__.py

--- plugins/Masterolic/__init__.py
+++ plugins/Masterolic/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from info import DB_URI
+from info import DATABASE_URI as DB_URI
 
 
 def start() -> scoped_session:
