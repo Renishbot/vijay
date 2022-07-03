07eeb2cfe52ef6a076acd4f7e6ad1346a0e76410
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-03 16:45:13 +0530

    Create carbon.py

---
+++ plugins/Naveen/carbon.py
@@ -0,0 +1,38 @@
+from pyrogram import filters
+from aiohttp import ClientSession
+from pyrogram import Client as bot
+from plugins.CrazyBoss.function import make_carbon
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+aiohttpsession = ClientSession()
+
+
+F = InlineKeyboardMarkup(
+[[
+     InlineKeyboardButton("Made By Me", url="https://t.me/VijayFilterTG_Bot")
+]]
+)
+
+
+
+
+@bot.on_message(filters.command("carbon"))
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
+        )
+    user_id = message.from_user.id
+    m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
+    C = f"**Carbon Made for {message.from_user.mention}**"
+    await message.reply_photo(
+        photo=carbon,
+        caption=C,
+        reply_markup=F)
+    await m.delete()
+    carbon.close()
