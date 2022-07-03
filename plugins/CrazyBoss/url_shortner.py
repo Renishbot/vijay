2bb4201de8eebaff0d6997119746eb06e99539a4
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-03 16:53:11 +0530

    Update url_shortner.py

--- plugins/CrazyBoss/url_shortner.py
+++ plugins/CrazyBoss/url_shortner.py
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nMade by {query.from_user.mention}"
+        shorten_urls += f"\n\nMade for {message.from_user.mention}"
         return shorten_urls
     except Exception as error:
         return error
