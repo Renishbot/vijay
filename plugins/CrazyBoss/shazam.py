6e97edec927c518cd39db7b11bdc640ae7545ee3
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-03 10:21:37 +0530

    Update shazam.py

--- plugins/CrazyBoss/shazam.py
+++ plugins/CrazyBoss/shazam.py
@@ -42,6 +42,20 @@ async def shazam(file):
     title = track.get("title")
     return image, by, title
 
+async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+    """Run Commands"""
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
 async def convert_to_audio(vid_path):
     stark_cmd = f"ffmpeg -i {vid_path} -map 0:a friday.mp3"
     await runcmd(stark_cmd)
