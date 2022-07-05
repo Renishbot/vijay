341d745dd7519e14ebd466a6378281d10b5762fc
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-05 14:01:40 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,5 +1,5 @@
 import pymongo
-from plugins.db import MONGODB_URL as db_url
+from plugins.db import MONGO_URL as db_url
 from plugins import help_message
 from pyrogram import Client
 from pyrogram import filters
