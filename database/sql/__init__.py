c62b8d8216e7d27327df8081e47cf241866ebcd0
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-05 10:14:06 +0530

    Update __init__.py

--- database/sql/__init__.py
+++ database/sql/__init__.py
@@ -1,4 +1,4 @@
-from info import DATABASE_URI
+from info import DATABASE_URI as DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
