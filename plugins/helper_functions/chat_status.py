6e6513d73a23519f4fbdaa6c0f1a37fe43ef629b
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-05 07:38:54 +0530

    Update chat_status.py

--- plugins/helper_functions/chat_status.py
+++ plugins/helper_functions/chat_status.py
@@ -309,5 +309,5 @@ def connection_status(func):
 
 
 #Workaround for circular import with connection.py
-from plugins import connection
+from plugins.connections import connection
 connected = connection.connected
