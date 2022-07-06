fe4ed62dd732387818ca7d5a5759efbf64015c9c
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-06 11:02:53 +0530

    Update rules.py

--- plugins/Group/rules.py
+++ plugins/Group/rules.py
@@ -63,8 +63,7 @@ async def get_rules(_, m: Message):
 
     formated = rules
 
-    await m.reply_text(
-        (tlang(m, "rules.get_rules")).format(
+    await m.reply_text("rules.get_rules")).format(
             chat=f"<b>{m.chat.title}</b>",
             rules=formated,
         ),
