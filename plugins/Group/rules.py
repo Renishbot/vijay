f6378505dc21eb05403d47daa4efb076582d12e4
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-06 10:41:27 +0530

    Update rules.py

--- plugins/Group/rules.py
+++ plugins/Group/rules.py
@@ -36,8 +36,7 @@ async def get_rules(_, m: Message):
         return
 
     if not rules:
-        await m.reply_text("rules.no_rules"),
-            quote=True
+        await m.reply_text("No Rules added in this Chat"),
         return
 
     priv_rules_status = db.get_privrules()
