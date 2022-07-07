7af4be73f8f632963116431ab8a5805f45d4b76c
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-07 14:25:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,8 +56,8 @@ async def start(client, message):
         await asyncio.sleep(2)
         await m.delete()
         await message.reply_chat_action("typing")
-        await message.reply_photo(
-            photo=random.choice(PICS),
+        await message.reply_video(
+            video="https://telegra.ph/file/464ed9d54beaaf8d89626.mp4",
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
