27d08136f59f2e76b80c15016ce9c53d567e9529
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-08 20:36:27 +0530

    Create basic_helpers.py

---
+++ plugins/helper_functions/basic_helpers.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+import math
+import os
+import shlex
+import time
+from math import ceil
+from traceback import format_exc
+from typing import Tuple
+from pyrogram import Client
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    Message,
+)
+
+async def edit_or_reply(message, text, parse_mode="md"):
+    sudo_lis_t = await sudo_list()
+    """Edit Message If Its From Self, Else Reply To Message, (Only Works For Sudo's)"""
+    if not message:
+        return await message.edit(text, parse_mode=parse_mode)
+    if not message.from_user:
+        return await message.edit(text, parse_mode=parse_mode)
+    if message.from_user.id in sudo_lis_t:
+        if message.reply_to_message:
+            return await message.reply_to_message.reply_text(text, parse_mode=parse_mode)
+        return await message.reply_text(text, parse_mode=parse_mode)
+    return await message.edit(text, parse_mode=parse_mode)
