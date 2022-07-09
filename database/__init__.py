c47f28d1bb08685e2b7019b9b40e448d86527665
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-09 15:15:55 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,8 @@
 import logging
-import motor.motor_asyncio
+import motor.motor_asyncio as AsyncIOMotorClient
 
-from info import Config
+from info import Config, DATABASE_URI
 
 mongo_client = motor.motor_asyncio.AsyncIOMotorClient(Config.DATABASE_URI)
 
-db_x = mongo_client["Friday"]
+db_x = mongo_client["Vijay"]
