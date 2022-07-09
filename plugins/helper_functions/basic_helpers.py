ddde5a8107d7c1f60d8d35a419de30ad3c8ee3dc
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-09 10:46:24 +0530

    Update basic_helpers.py

--- plugins/helper_functions/basic_helpers.py
+++ plugins/helper_functions/basic_helpers.py
@@ -29,3 +29,16 @@ async def edit_or_reply(message, text, parse_mode="md"):
             return await message.reply_to_message.reply_text(text, parse_mode=parse_mode)
         return await message.reply_text(text, parse_mode=parse_mode)
     return await message.edit(text, parse_mode=parse_mode)
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
