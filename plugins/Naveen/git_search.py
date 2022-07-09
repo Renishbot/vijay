e5e9343976d564d3341a9b8b086471ee8241c440
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-09 17:17:54 +0530

    Update git_search.py

--- plugins/Naveen/git_search.py
+++ plugins/Naveen/git_search.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 from plugins.helper_functions.basic_helpers import edit_or_reply, get_text
 
 
-@Client.on_message(filters.command('git'))
+@Client.on_message(filters.command('repo'))
 async def git(client, message):
     engine = message.Engine
     pablo = await edit_or_reply(message, engine.get_string("PROCESSING"))
