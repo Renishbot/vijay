b143968ada2386fe699b2ca4151d845a8604486d
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-10 10:33:04 +0530

    Update ifsc.py

--- plugins/Naveen/ifsc.py
+++ plugins/Naveen/ifsc.py
@@ -5,7 +5,7 @@ from plugins.helper_functions.basic_helpers import edit_or_reply, get_text, edit
 
 @Client.on_message(filters.command('ifsc'))
 async def geT_if(client, message):
-    m_ = await edit_or_reply(message, "`Please Wait!`")
+    m_ = await message.reply_text("Please Wait!")
     input_str = get_text(message)
     if not input_str:
         return await m_.edit("Give Me IFSC Code As Input.")
