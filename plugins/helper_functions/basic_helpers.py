7127c1d63be6e869042163b7cee758811d4ceaab
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-10 08:55:49 +0530

    Update basic_helpers.py

--- plugins/helper_functions/basic_helpers.py
+++ plugins/helper_functions/basic_helpers.py
@@ -43,3 +43,27 @@ def get_text(message: Message) -> [None, str]:
             return None
     else:
         return None
+
+async def edit_or_send_as_file(
+    text: str,
+    message: Message,
+    client: Client,
+    caption: str = "`Result!`",
+    file_name: str = "result",
+    parse_mode="md",
+):
+    """Send As File If Len Of Text Exceeds Tg Limit Else Edit Message"""
+    if not text:
+        await message.edit("`Wait, What?`")
+        return
+    if len(text) > 1024:
+        await message.edit("`OutPut is Too Large, Sending As File!`")
+        file_names = f"{file_name}.text"
+        open(file_names, "w").write(text)
+        await client.send_document(message.chat.id, file_names, caption=caption)
+        await message.delete()
+        if os.path.exists(file_names):
+            os.remove(file_names)
+        return
+    else:
+        return await message.edit(text, parse_mode=parse_mode)
