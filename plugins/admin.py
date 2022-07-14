39a90b7b5ccfe792881772e549c7eee9e198a9ee
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-14 20:57:28 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -31,7 +31,7 @@ from plugins.Group.utils.custom_filters import (
 from plugins.Group.utils.extract_user import extract_user
 from plugins.Group.utils.parser import mention_html
 from info import Config
-
+from Wbb.permission import adminsOnly
 
 @Alita.on_message(command("adminlist"))
 async def adminlist_show(_, m: Message):
@@ -167,6 +167,49 @@ async def tag_admins(_, m: Message):
             f" reported the message to admins!{mention_str}"
         ),
     )
+
+@Alita.on_message(
+    filters.command(["promote", "fullpromote"])
+    & ~filters.edited
+    & ~filters.private
+)
+@adminsOnly("can_promote_members")
+async def promoteFunc(_, message: Message):
+    user_id = await extract_user(message)
+    umention = (await app.get_users(user_id)).mention
+    if not user_id:
+        return await message.reply_text("I can't find that user.")
+    bot = await app.get_chat_member(message.chat.id, BOT_ID)
+    if user_id == BOT_ID:
+        return await message.reply_text("I can't promote myself.")
+    if not bot.can_promote_members:
+        return await message.reply_text("I don't have enough permissions")
+    if message.command[0][0] == "f":
+        await message.chat.promote_member(
+            user_id=user_id,
+            can_change_info=bot.can_change_info,
+            can_invite_users=bot.can_invite_users,
+            can_delete_messages=bot.can_delete_messages,
+            can_restrict_members=bot.can_restrict_members,
+            can_pin_messages=bot.can_pin_messages,
+            can_promote_members=bot.can_promote_members,
+            can_manage_chat=bot.can_manage_chat,
+            can_manage_voice_chats=bot.can_manage_voice_chats,
+        )
+        return await message.reply_text(f"Fully Promoted! {umention}")
+
+    await message.chat.promote_member(
+        user_id=user_id,
+        can_change_info=False,
+        can_invite_users=bot.can_invite_users,
+        can_delete_messages=bot.can_delete_messages,
+        can_restrict_members=False,
+        can_pin_messages=False,
+        can_promote_members=False,
+        can_manage_chat=bot.can_manage_chat,
+        can_manage_voice_chats=bot.can_manage_voice_chats,
+    )
+    await message.reply_text(f"Promoted! {umention}")
 
 
 @Alita.on_message(command("demote") & promote_filter)
