f9b17ce9e5daa5c424550ef0422cf1464860b606
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-14 21:40:15 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -33,32 +33,6 @@ from plugins.Group.utils.parser import mention_html
 from info import Config
 from Wbb.permissions import adminsOnly
 
-async def member_permissions(chat_id: int, user_id: int):
-    perms = []
-    try:
-        member = await app.get_chat_member(chat_id, user_id)
-    except Exception:
-        return []
-    if member.can_post_messages:
-        perms.append("can_post_messages")
-    if member.can_edit_messages:
-        perms.append("can_edit_messages")
-    if member.can_delete_messages:
-        perms.append("can_delete_messages")
-    if member.can_restrict_members:
-        perms.append("can_restrict_members")
-    if member.can_promote_members:
-        perms.append("can_promote_members")
-    if member.can_change_info:
-        perms.append("can_change_info")
-    if member.can_invite_users:
-        perms.append("can_invite_users")
-    if member.can_pin_messages:
-        perms.append("can_pin_messages")
-    if member.can_manage_voice_chats:
-        perms.append("can_manage_voice_chats")
-    return perms
-
 
 @Alita.on_message(command("adminlist"))
 async def adminlist_show(_, m: Message):
