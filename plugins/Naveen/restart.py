0db68b1874efec66ece0815d75e1a969e418b531
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-19 17:03:38 +0530

    Create restart.py

---
+++ plugins/Naveen/restart.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+from pyrogram.raw.functions import Ping
+from mbot import LOG_GROUP, OWNER_ID, SUDO_USERS, Mbot,AUTH_CHATS
+from os import execvp,sys
