c8be6ec61edba70c8346ce82a61ca969b079f03a
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-19 17:06:12 +0530

    Update restart.py

--- plugins/Naveen/restart.py
+++ plugins/Naveen/restart.py
@@ -1,6 +1,13 @@
 from datetime import datetime
-from pyrogram import filters
+from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 from pyrogram.raw.functions import Ping
-from mbot import LOG_GROUP, OWNER_ID, SUDO_USERS, Mbot,AUTH_CHATS
+from info import LOG_GROUP, OWNER_ID, SUDO_USERS,
 from os import execvp,sys
+
+
+
+@Client.on_message(filters.command("restart") & filters.chat(OWNER_ID) & filters.private)
+async def restart(_,message):
+    await message.delete()
+    execvp(sys.executable,[sys.executable,"-m","Client"])
