e9fbd9d15d0ed6cb858cc5d1a08597b929717bff
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-19 20:10:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -221,7 +221,8 @@ class Config((object)):
         "UPSTREAM_REPO", "https://github.com/Naveen-TG/MasterolicTG"
     )
     U_BRANCH = "main"
-    HEROKU_APP_NAME = environ.get("HEROKU_APP_NAME", None)
+    HEROKU_APP_NAME = environ.get("HEROKU_APP_NAME", "")
+    HEROKU_API_KEY = environ.get("HEROKU_API_KEY", "")
 
 OWNER_ID = Config.OWNER_ID
 DEV_USERS = Config.DEV_USERS
