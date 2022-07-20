5d70ddf73be8ce30ed0b2e6b1282f9405de47a71
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-07-20 10:10:28 +0530

    Update restart.py

--- plugins/Naveen/restart.py
+++ plugins/Naveen/restart.py
@@ -2,12 +2,12 @@ from datetime import datetime
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 from pyrogram.raw.functions import Ping
-from info import OWNER_ID, SUDO_USERS
+from info import ADMINS, SUDO_USERS
 from os import execvp,sys,execl
 from sys import executable
 
 
-@Client.on_message(filters.command("restart") & filters.chat(OWNER_ID)
+@Client.on_message(filters.command("restart") & filters.chat(ADMINS)
 async def restart(_,message):
     try:
         await message.delete()
