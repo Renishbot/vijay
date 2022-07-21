c4b63f0d319cca1b294175303ffe2daba1e4ef3d
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-07-21 09:59:27 +0530

    Update restart.py

--- plugins/Naveen/restart.py
+++ plugins/Naveen/restart.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 from pyrogram.raw.functions import Ping
-from info import ADMINS, SUDO_USERS
+from info import ADMINS, SUDO_USERS, DEV_USERS
 from os import execvp,sys,execl
 from sys import executable
 
@@ -10,7 +10,7 @@ from sys import executable
 @Client.on_message(filters.command("restart") & ~filters.edited)
 async def restart(_,message):
     try:
-        if message.from_user.id not in ADMINS:
+        if message.from_user.id not in DEV_USERS:
            await message.delete()
            await message.reply("Only bot admins can do this action⚠️") 
            return
