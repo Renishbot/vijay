c47bc610832cb1a09a041e173aec64ee2abc150c
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-28 19:04:52 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,20 @@
 import motor
 import logging
 import motor.motor_asyncio as AsyncIOMotorClient
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from info import Config, DATABASE_URI as DB_URI
 
-from info import Config, DATABASE_URI
-
-mongo_client = motor.motor_asyncio.AsyncIOMotorClient(Config.DATABASE_URI)
+mongo_client = motor.motor_asyncio.AsyncIOMotorClient(Config.DB_URI)
 
 db_x = mongo_client["Vijay"]
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+BASE = declarative_base()
+SESSION = start()
