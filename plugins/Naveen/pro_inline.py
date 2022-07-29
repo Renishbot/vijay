de35843294b6b02b3c23a363fb27853a506f8821
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-29 18:44:14 +0530

    Update pro_inline.py

--- plugins/Naveen/pro_inline.py
+++ plugins/Naveen/pro_inline.py
@@ -5,12 +5,22 @@ from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessa
 async def inlinemode(bot, query: InlineQuery):
     await query.answer(
         results=[
+
             InlineQueryResultArticle(
                 title="Naveen",
                 description="Click Here",
                 thumb_url="https://telegra.ph/file/a91ddf16775ec8ef0bd9e.jpg",
                 input_message_content=InputTextMessageContent(
                     message_text="""Owner of the Bot"""
+                )
+            ),
+
+            InlineQueryResultArticle(
+                title="About the Bot",
+                description="Show the Details of the Bot and Show the Purpose of Vijay BoT",
+                thumb_url="https://telegra.ph/file/a91ddf16775ec8ef0bd9e.jpg",
+                input_message_content=InputTextMessageContent(
+                    message_text="""Here is the All Details of the BoT"""
                 )
             )
         ],
