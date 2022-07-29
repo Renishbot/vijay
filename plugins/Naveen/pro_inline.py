56af8f1f0cf1d1d2cf9aafaa2dc2c1278e44ac0b
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-29 17:49:27 +0530

    Update pro_inline.py

--- plugins/Naveen/pro_inline.py
+++ plugins/Naveen/pro_inline.py
@@ -9,7 +9,7 @@ async def inlinemode(bot, query: InlineQuery):
                 title="Naveen",
                 description="Click Here",
                 thumb_url="https://telegra.ph/file/a91ddf16775ec8ef0bd9e.jpg",
-                input_message_contect=InputTextMessageContent(
+                input_message_content=InputTextMessageContent(
                     message_text="""Owner of the Bot"""
                 )
             )
