63ea57674e28e953d9e976f054a6e467b71bb05d
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-30 09:21:26 +0530

    Update pro_inline.py

--- plugins/Naveen/pro_inline.py
+++ plugins/Naveen/pro_inline.py
@@ -9,7 +9,7 @@ async def inlinemode(bot, query: InlineQuery):
             InlineQueryResultArticle(
                 title="About BOT",
                 description="Buttoned Details of Vijay will be Shown Here, Try it❤️",
-                thumb_url="https://telegra.ph/file/a91ddf16775ec8ef0bd9e.jpg",
+                thumb_url="https://telegra.ph/file/75c9be07bb6e2a9309041.jpg",
                 input_message_content=InputTextMessageContent(
                     message_text="""Owner of the Bot"""
                 ),
@@ -23,7 +23,7 @@ async def inlinemode(bot, query: InlineQuery):
             InlineQueryResultArticle(
                 title="About the Bot",
                 description="Show the Details of the Bot and Show the Purpose of Vijay BoT",
-                thumb_url="https://telegra.ph/file/75c9be07bb6e2a9309041.jpg",
+                thumb_url="https://telegra.ph/file/a91ddf16775ec8ef0bd9e.jpg",
                 input_message_content=InputTextMessageContent(
                     message_text="""Here is the All Details of the BoT"""
                 )
