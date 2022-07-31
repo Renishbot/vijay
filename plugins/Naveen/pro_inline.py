fc878fcf35d19167f7b4a83b49c7116dbfd254b6
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-07-31 09:32:34 +0530

    Update pro_inline.py

--- plugins/Naveen/pro_inline.py
+++ plugins/Naveen/pro_inline.py
@@ -20,14 +20,14 @@ async def inlinemode(bot, query: InlineQuery):
                 )
             )
 
-            #InlineQueryResultArticle(
-                #title="About the Bot",
-                #description="Show the Details of the Bot and Show the Purpose of Vijay BoT",
-                #thumb_url="https://telegra.ph/file/75c9be07bb6e2a9309041.jpg",
-                #input_message_content=InputTextMessageContent(
-                    #message_text="""Here is the All Details of the BoT"""
-                #)
-            #)
+            InlineQueryResultArticle(
+                title="About the Bot",
+                description="Show the Details of the Bot and Show the Purpose of Vijay BoT",
+                thumb_url="https://telegra.ph/file/75c9be07bb6e2a9309041.jpg",
+                input_message_content=InputTextMessageContent(
+                    message_text="""Here is the All Details of the BoT"""
+                )
+            )
         ],
         cache_time=0
     )
