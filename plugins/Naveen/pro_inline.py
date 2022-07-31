6c6ab5cf63700c57cd0f6af29ff5926963c8fb6f
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-07-31 15:28:51 +0530

    Update pro_inline.py

--- plugins/Naveen/pro_inline.py
+++ plugins/Naveen/pro_inline.py
@@ -14,8 +14,8 @@ async def inlinemode(bot, query: InlineQuery):
                     message_text="""Owner of the Bot"""
                 ),
                 reply_markup=InlineKeyboardMarkup([[
-                    InlineKeyboardButton('🛠️ 𝙃𝙚𝙡𝙥', callback_data='helpq'),
-                    InlineKeyboardButton('⭕️ 𝘼𝙗𝙤𝙪𝙩', callback_data='aboutq')
+                    InlineKeyboardButton('🛠️ 𝙃𝙚𝙡𝙥', callback_data='inline_help'),
+                    InlineKeyboardButton('⭕️ 𝘼𝙗𝙤𝙪𝙩', callback_data='inline_about')
                     ]]
                 )
             ),
