7cd4d2f6b3fc8cda9da956922f3a1ddf2411c9ee
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-08-03 18:56:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1063,7 +1063,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-     elif query.data == "country":
+    elif query.data == "country":
         buttons = [[
             InlineKeyboardButton('« 𝐵𝑎𝑐𝑘', callback_data='help')
         ]]
